4abcfec7388b8bf6a077035dd543753923f55d0d
Abhay Singh <128287855+dhiyochamp@users.noreply.github.com>
2025-04-06 13:20:18 +0530

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -53,6 +53,24 @@
         }
     });
     
+    document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', revealOnScroll);
+    revealOnScroll();
+    
+    function revealOnScroll() {
+        const reveals = document.querySelectorAll('.reveal');
+        
+        for (let i = 0; i < reveals.length; i++) {
+            const windowHeight = window.innerHeight;
+            const revealTop = reveals[i].getBoundingClientRect().top;
+            const revealPoint = 150;
+            
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add('active');
+            }
+        }
+    }
+});
     
     // Back to top button
     $(window).scroll(function () {
